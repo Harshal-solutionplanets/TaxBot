badd767d47c7d900da40aaba54aac16fe23d3d43
taxbot0807-rgb <taxbot0807@gmail.com>
2026-07-15 15:13:12 +0530

    Allow CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ CACHE_TTL_SECONDS = 24 * 60 * 60
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
